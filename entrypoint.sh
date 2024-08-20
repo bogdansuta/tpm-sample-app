a2839292dce75aa303ac99f088097f75f2a8d981
Bogdan Suta <bogdan@getxp.dev>
2024-08-20 14:16:28 +0300

    Adjusted Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ COMMAND=$1
 
 case "${COMMAND}" in
     *)
+        echo "Running migrations"
         /code/manage.py migrate
+        echo "Running server"
         /code/manage.py runserver 0.0.0.0:8000
 esac
